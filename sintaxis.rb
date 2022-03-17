9f294c85c34ec72d7a672b8985b0833ed543e641
recenama <cajilemapaola@gmail.com>
2022-03-17 15:58:05 -0500

    feat: sintaxis

---
+++ sintaxis.rb
@@ -0,0 +1,9 @@
+# not necesary ;
+# for comment block code, add without blank spaces "=begin //code =end"
+=begin name = "Pao"
+preferred_name = "Pau"
+=end
+email = "pau@gmail.com"
+
+# dont use parenthesis with DSL or int test
+# get '/login' do
