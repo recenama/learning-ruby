49197e6902d7e7afc0e60a7d7bbe4cdc22847ba7
recenama <cajilemapaola@gmail.com>
2022-03-17 16:16:20 -0500

    feat: arithmetic operators

---
+++ arithmetic_operators.rb
@@ -0,0 +1,21 @@
+x = 5
+y = 3
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x.to_f / y.to_f 
+modulus = x % y
+exponentiation = x ** y
+
+puts addition
+puts subtraction
+puts multiplication
+puts division
+puts modulus
+puts exponentiation
+
+# operator's order
+#   1. **
+#   2. * / %
+#   3. + -
