85510d5428a39da08a376a03cc1588cec34a1c5e
recenama <cajilemapaola@gmail.com>
2022-03-18 10:52:20 -0500

    feat: arrays

---
+++ arrays.rb
@@ -0,0 +1,10 @@
+first_array = [1,'Pao']
+second_array = Array.new(3)
+first_array[2] = 'Rio'
+
+third_array = %w[1 'Riobamba' 'canton'] # in this ways is not neecesary comas
+third_array << 'Chimborazo'
+
+puts first_array
+puts second_array
+puts third_array
