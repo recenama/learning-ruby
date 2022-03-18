40f365707fe7b149b97c069d8c6b790a01b607cb
recenama <cajilemapaola@gmail.com>
2022-03-18 11:22:39 -0500

    feat: operations with arrays

--- arrays.rb
+++ arrays.rb
@@ -23,3 +23,14 @@ qualifications.each_with_index do |qualification, position|
 end
 
 puts "Average of your qualifications: #{sum.to_f/qualifications.length}"
+
+# common operations with arrays
+second_qualifications = [17, 15, 19, 19, 20] #are numbers
+puts second_qualifications * "-" # joining with dash 
+puts second_qualifications.sort # sorting asc
+puts second_qualifications.sort.reverse # sorting desc
+puts second_qualifications.include?(20) # find an element
+puts second_qualifications.first
+puts second_qualifications.last
+puts second_qualifications.uniq #array with no repeated elements 
+puts second_qualifications.sample # return an element from array in a random way
