de797c61f8e729d35fe2f0366df447d38729e6eb
recenama <cajilemapaola@gmail.com>
2022-03-21 11:33:56 -0500

    feat: operations with hash

--- hash.rb
+++ hash.rb
@@ -5,3 +5,19 @@ puts professor[:name]
 professor.each do |key, value|
     puts "In #{key} is saving #{value}"
 end
+
+puts professor.length
+puts professor.size
+
+puts professor.has_key?(:name)
+puts professor.has_value?("Pau")
+puts professor.keys
+puts professor.values
+puts professor.key("Pau")
+puts professor.invert # transform value -> key and key -> value
+
+professor.delete(:subjects)
+puts professor
+professor.clear
+puts professor.empty?
+
