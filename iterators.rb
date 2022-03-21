00eb9fd9845684707633e07124da0ab791db149e
recenama <cajilemapaola@gmail.com>
2022-03-21 08:01:05 -0500

    feat: more iterators

---
+++ iterators.rb
@@ -0,0 +1,17 @@
+# Times, upto downto
+
+10.times do |i|
+    puts i
+end
+
+10.times do
+    puts "running is not necessary iterator variable"
+end
+
+1.upto(10) do |i|
+    puts i
+end
+
+8.downto(1) do |i|
+    puts i
+end
